2a2b7de90be561ca4a349cc93c16294e6c3eaf4a
luca <luca.errani1@gmail.com>
2019-09-10 12:36:34 +0200

    Prova react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TextBox from './TextBox';
+import 'tachyons';
+import { greetings } from './greetings';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            textInput: '',
+        };
+    }
+
+    isPalindrome = (string) => {
+        if (string.length < 1) {
+            return false;
+        }
+        string = string.replace(/ /g, '');
+        for (let i = 0, j = string.length - 1; i < string.length; i++ , j--) {
+            if (string[i] !== string[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    onInputChange = (event) => {
+        this.setState({ textInput: event.target.value });
+    }
+
+    render() {
+
+        // When the page gets re-rendered, we check wether or not the word is palindrome
+        // and tell the TextBox component
+        let borderColor;
+        let showGreeting;
+        if (this.isPalindrome(this.state.textInput.toLowerCase())) {
+            borderColor = "b--green";
+            showGreeting = greetings[Math.floor(Math.random() * 100) % greetings.length];
+            console.log(showGreeting);
+
+        } else {
+            borderColor = "b--red";
+        }
+
+        return (
+            <div className='tc ma4 pa3'>
+                <TextBox
+                    textInputChange={this.onInputChange}
+                    border={borderColor}
+                    greeting={showGreeting} />
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/greetings.js
@@ -0,0 +1,11 @@
+export const greetings = [
+    "What a man!",
+    "You're sooo good",
+    "!!Unbelievable!!",
+    "I wasn't expecting that",
+    "That's unusual",
+    "TERRRRIFIC",
+    "**omg**",
+    "Awesome!",
+    "Did you invented that??"
+];
